ff2b45463257dbbe17b557902af13ebeedcfe325
bruce-mig <brucetmig@icloud.com>
2024-12-16 11:01:13 +0200

    removed lombok annotations

--- src/main/java/com/github/bruce_mig/spring_reactive_web/student/Student.java
+++ src/main/java/com/github/bruce_mig/spring_reactive_web/student/Student.java
@@ -1,15 +1,12 @@
 package com.github.bruce_mig.spring_reactive_web.student;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Data
-@NoArgsConstructor
+
 @Builder
 @Table(name = "students")
 public class Student {
@@ -27,5 +24,50 @@ public class Student {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
