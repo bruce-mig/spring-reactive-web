61357da8dc8f4738de6c3f45518ecf9afbb10877
bruce-mig <brucetmig@icloud.com>
2024-12-16 11:01:48 +0200

    added Repository annotation

--- src/main/java/com/github/bruce_mig/spring_reactive_web/student/StudentRepository.java
+++ src/main/java/com/github/bruce_mig/spring_reactive_web/student/StudentRepository.java
@@ -1,8 +1,10 @@
 package com.github.bruce_mig.spring_reactive_web.student;
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
+@Repository
 public interface StudentRepository extends ReactiveCrudRepository<Student, Integer> {
 
     Flux<Student> findAllByFirstNameIgnoreCase(String firstName);
